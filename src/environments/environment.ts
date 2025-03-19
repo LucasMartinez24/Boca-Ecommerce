748720716f8df43d2f5ac9bb882ca8ea0123d6e6
Lucas Salvador Martinez <102763328+LucasMartinez24@users.noreply.github.com>
2025-03-19 03:29:45 -0300

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,11 +1,12 @@
 export const environment={
   firebase: {
-    projectId: 'encanto-c2fef',
-    appId: '1:875741320238:web:5d1cc80e8c1d254d177bbf',
-    storageBucket: 'encanto-c2fef.appspot.com',
-    apiKey: 'AIzaSyAmgrjXoJkJl-ukeHJO5gS0i-TWR5f_-iI',
-    authDomain: 'encanto-c2fef.firebaseapp.com',
-    messagingSenderId: '875741320238',
+    apiKey: "AIzaSyBbdvRl7JXOgN32pRSSsTJgf4oK-W9ppLA",
+  authDomain: "boca-ecommerce-43def.firebaseapp.com",
+  projectId: "boca-ecommerce-43def",
+  storageBucket: "boca-ecommerce-43def.firebasestorage.app",
+  messagingSenderId: "684371694867",
+  appId: "1:684371694867:web:129503686ce3671938b6e6",
+  measurementId: "G-M5828D7G2B",
     ignoreUndefinedProperties: true,
   },
   production:false,
